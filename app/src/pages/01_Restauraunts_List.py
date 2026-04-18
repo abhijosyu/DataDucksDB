11086ed42fc3f7350c642370b2d1b0144c783138
abhijosyu <abhijosyu@gmail.com>
2026-04-17 20:17:03 -0400

    added basic route functions for reviews + some of owner

--- app/src/pages/01_Restauraunts_List.py
+++ app/src/pages/01_Restauraunts_List.py
@@ -7,6 +7,8 @@ import numpy as np
 import plotly.express as px
 from modules.nav import SideBarLinks
 
+import requests
+
 st.set_page_config(layout='wide')
 
 # Call the SideBarLinks from the nav module in the modules directory
@@ -14,5 +16,23 @@ SideBarLinks()
 
 # set the header of the page
 st.markdown('# List Of Restaurants')
+
+def get_locations():
+    try:
+        response = requests.get("http://127.0.0.1:5000/locations") # change route once done
+        return response.json()
+    except requests.exceptions.RequestException:
+        return []
+
+locations = get_locations()
+
+if not locations:
+    st.warning("No restaurant locations found.")
+else:
+    for loc in locations:
+        st.subheader(loc["name"])
+        st.write(f'City: {loc["city"]}')
+        st.write(f'Address: {loc["address"]}')
+        st.divider()
 
 
