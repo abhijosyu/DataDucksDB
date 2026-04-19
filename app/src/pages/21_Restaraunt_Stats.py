6e9931f71d886af28e3d9200dd78d7fec6c29abb
abhijosyu <abhijosyu@gmail.com>
2026-04-19 13:20:28 -0400

    added restauraunt_list for owners

--- app/src/pages/21_Restaraunt_Stats.py
+++ app/src/pages/21_Restaraunt_Stats.py
@@ -1,7 +1,5 @@
 import logging
-
 logger = logging.getLogger(__name__)
-
 import streamlit as st
 from modules.nav import SideBarLinks
 import requests
@@ -13,33 +11,5 @@ SideBarLinks()
 
 st.title("Location Stats")
 
-company_id = 7  # example
-
-def get_company_locations(company_id):
-    response = requests.get(f"http://127.0.0.1:5000/companies/{company_id}/locations")
-    response.raise_for_status()
-    return response.json()
-
-locations = get_company_locations(company_id)
 
 
-selected_location = st.selectbox(
-    "Choose a restaurant location",
-    locations,
-    format_func=lambda loc: f'{loc["name"]} - {loc["city"]}'
-)
-
-
-def get_location_analytics(location_id):
-    response = requests.get(f"http://127.0.0.1:5000/analytics/location/{location_id}")
-    response.raise_for_status()
-    return response.json()
-
-if selected_location:
-    analytics = get_location_analytics(selected_location["location_id"])
-
-    st.subheader("Location Analytics")
-    st.write("Average Rating:", analytics["average_rating"])
-    st.write("Review Count:", analytics["review_count"])
-    st.write("Trend:", analytics["positive_trend"])
-    st.write("Common Complaint:", analytics["common_complaint"])
--- app/src/pages/24_Owner_Restauraunts.py
+++ app/src/pages/24_Owner_Restauraunts.py
@@ -5,20 +5,46 @@ import pandas as pd
 import pydeck as pdk
 from urllib.error import URLError
 from modules.nav import SideBarLinks
+import requests
 
 st.set_page_config(layout='wide')
 
 SideBarLinks()
 
 # set up the page
-st.markdown("# Restaurant List")
 
 
-company = "Chipotle"  # example
+API_BASE = "http://127.0.0.1:4000/api/company"
 
-response = requests.get(
-    "http://127.0.0.1:5000/locations",
-    params={"company": company}
-)
 
-locations = response.json()
+company_id = 7  
+
+def get_company_name(company_id):
+    response = requests.get(f"{API_BASE}/{company_id}")
+    response.raise_for_status()
+    return response.json()
+
+def get_company_locations(company_id):
+    response = requests.get(f"{API_BASE}/{company_id}/locations")
+    response.raise_for_status()
+    return response.json()
+
+locations = get_company_locations(company_id)
+
+name = get_company_name(company_id)
+st.markdown(f'# {name["name"]} Restauraunts List')
+for loc in locations:
+        st.markdown(loc)
+
+        col1, col2 = st.columns([4, 1])
+
+        with col1:
+            st.caption(f"{loc['address']}, {loc['city']}")
+
+        with col2:
+            
+            if st.button("View", key=f"view_{loc}"):
+                st.session_state["selected_location"] = loc
+                st.switch_page("pages/21_Restaraunt_Stats.py")
+
+        st.divider()
